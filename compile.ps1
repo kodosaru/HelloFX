8547d66290b14aaecf4fa466659e6d448db695e5
Don Johnson <kodosaru@gmail.com>
2021-07-29 12:40:56 -0400

    Initial commit

---
+++ compile.ps1
@@ -0,0 +1,1 @@
+javac --module-path "\opt\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml .\HelloFX.java
---
+++ run.ps1
@@ -0,0 +1,1 @@
+java --module-path "\opt\javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml HelloFX
